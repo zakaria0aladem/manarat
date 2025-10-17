c16bf18d5f357f6dd63e2df0b8077214ca1851c2
Zakaria Aladem <zakarialadem2@gmail.com>
2025-10-17 17:37:47 +0300

    Update sending time and add new daily message

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from telegram import Bot
 import asyncio
 from datetime import datetime
+import os
 import csv
 
-BOT_TOKEN = "8415783103:AAFyQi7w05NDSlaxwW6u5-MJfowavN9l_5A"
-CHANNEL_ID = "-1002618330740"  # or "@YourChannelUsername"
-CSV_FILE = r"C:\Users\zakar\Downloads\test - Sheet1.csv"
 
-# Load messages from CSV
+BOT_TOKEN = os.environ["8415783103:AAFyQi7w05NDSlaxwW6u5-MJfowavN9l_5A"]
+CHANNEL_ID = os.environ["-1002618330740"]
+
+
+# For Railway, put the CSV in your project folder and use relative path
+CSV_FILE = r"C:\Users\zakar\OneDrive - Balqa Applied University\Desktop\Z\test - Sheet1.csv"  # place your CSV in the same folder as this script
+
+
 def load_messages():
     messages = []
     with open(CSV_FILE, newline="", encoding="utf-8") as csvfile:
@@ -16,27 +21,30 @@ def load_messages():
             messages.append(row["message"])
     return messages
 
+
 async def send_message(message: str):
     bot = Bot(token=BOT_TOKEN)
     date = datetime.now().strftime("%Y-%m-%d")
     full_message = f"{message}\n🕰️ التاريخ: {date}"
     try:
         await bot.send_message(chat_id=CHANNEL_ID, text=full_message)
-        print(" Message sent:", full_message)
+        print("Message sent:", full_message)
     except Exception as e:
-        print(" Failed to send message:", e)
+        print("Failed to send message:", e)
+
 
 async def scheduler():
     messages = load_messages()
     while True:
         now = datetime.now()
-        # Change hour and minute to your desired sending time
-        if now.hour == 15 and now.minute == 55:
+        # Set the time you want the message to be sent
+        if now.hour == 17 and now.minute == 45:
             # Pick message based on the day number
             day_index = (now.toordinal() - 738156) % len(messages)
             await send_message(messages[day_index])
             await asyncio.sleep(60)  # avoid sending twice in the same minute
-        await asyncio.sleep(1)
+        await asyncio.sleep(20)
+
 
 if __name__ == "__main__":
     asyncio.run(scheduler())
